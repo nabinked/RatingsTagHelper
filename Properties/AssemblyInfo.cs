0394a1435063311066e6ef514d2f0f9ad2554165
Nabin Karki Thapa <nabin@outlook.com>
2016-01-25 09:57:09 +1100

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RatingsTagHelper")]
+[assembly: AssemblyDescription("ASP.NET TagHelper for Ratings")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RatingsTagHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Nabin Karki Thapa")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21828340-53d2-4579-a0bc-b7c452c1ae5d")]
---
+++ RatingsTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace RatingsTagHelper
+{
+    [HtmlTargetElement("rating")]
+    public class RatingsTagHelper : TagHelper
+    {
+        public int Rating { get; set; }
+        public string SizeClass { get; set; }
+
+        private HtmlString GetContent()
+        {
+            var sb = new StringBuilder();
+            sb.Append("<div>");
+            for (int i = 1; i < 6; i++)
+            {
+                
+                sb.Append(i <= Rating ? GetStarDiv("rating-full") : GetStarDiv("rating-empty"));
+            }
+            sb.Append("</div>");
+            var html = new HtmlString(sb.ToString());
+            return html;
+        }
+
+        private string GetStarDiv(string ratingClass)
+        {
+            return $"<div class='{ratingClass} {SizeClass}'></div>";
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            var content = GetContent();
+            output.Content.SetContent(content);
+
+            base.Process(context, output);
+        }
+    }
+}
